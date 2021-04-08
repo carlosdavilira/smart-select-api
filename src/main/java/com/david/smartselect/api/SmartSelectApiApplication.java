3adf39f0afc385055f8ddb44eec004727a674fc6
Carlos David <carlosdavilira@gmail.com>
2021-04-08 02:15:41 -0300

    [SmartSelect][Add] Updaload project and create migration 001 - create user table

---
+++ src/main/java/com/david/smartselect/api/SmartSelectApiApplication.java
@@ -0,0 +1,13 @@
+package com.david.smartselect.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSelectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSelectApiApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V001__Create_User_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE usuario (
+codigo BIGINT(20) primary key auto_increment,
+usuario varchar(50) not null,
+senha varchar(12) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
