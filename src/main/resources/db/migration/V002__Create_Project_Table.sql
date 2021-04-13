b5d27b6f7782d2da878e8edf80267cf8a88fa102
Carlos David <carlosdavilira@gmail.com>
2021-04-13 16:25:22 -0300

    [SmartSelect][Fix] Project class and migration names.

--- src/main/resources/db/migration/V002__Create_Project_Table.sql
+++ src/main/resources/db/migration/V002__Create_Project_Table.sql
@@ -2,5 +2,5 @@ CREATE TABLE projeto (
 codigo BIGINT(20) primary key auto_increment,
 descricao LONGTEXT not null,
 habilidades LONGTEXT not null,
-tempos_habilidades LONGTEXT not null
+tempos LONGTEXT not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
