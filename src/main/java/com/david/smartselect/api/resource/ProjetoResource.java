bae7d488312d8d8aa6b5a8ad7b87eba88333e25a
Carlos David <carlosdavilira@gmail.com>
2021-07-01 16:58:47 -0300

    [SmartSelect][ADD] Add CORS to GET project

--- src/main/java/com/david/smartselect/api/resource/ProjetoResource.java
+++ src/main/java/com/david/smartselect/api/resource/ProjetoResource.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,7 @@ public class ProjetoResource {
 		return projetoService.listar();
 	}
 	
+	@CrossOrigin(maxAge= 10, origins = {"*"})
 	@GetMapping("/{codigo}")
 	public ResponseEntity buscarPeloCodigo(@PathVariable Long codigo) {				
 		return projetoService.buscarPeloCodigo(codigo);
